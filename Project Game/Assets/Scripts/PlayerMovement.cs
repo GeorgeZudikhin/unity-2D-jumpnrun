911d65561619bbcf16464d16d9c5f4ede0fb7566
CaptainKnobel <cklein@live.at>
2023-04-15 16:01:13 +0200

    Animations, Crouching, Jumping
    Added animator with a wide set of animations to the player character and introduced jumping and crouching functionality.
    Side Node: When jumping you get stuck at vertical walls.
    Tileset physics aren't fully converted to flat surfaces yet. (stone to the right of the bridge still bugs around.

--- Project Game/Assets/Scripts/PlayerMovement.cs
+++ Project Game/Assets/Scripts/PlayerMovement.cs
@@ -5,19 +5,52 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public CharacterController2D controller;
+    public Animator animator;
     
     public float runSpeed = 40f;
 
     float horizontalMove = 0f;
+    bool jump = false;
+    bool crouch = false;
 
     // Update is called once per frame
     void Update()
     {
         horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+            animator.SetBool("IsJumping", true);
+        }
+
+        if(Input.GetButtonDown("Crouch"))
+        {
+            crouch = true;
+            //animator.SetBool("IsCrouching", true);
+        }
+        else if(Input.GetButtonUp("Crouch"))
+        {
+            crouch = false;
+            //animator.SetBool("IsCrouching", false);
+        }
+    }
+
+    public void OnLanding()
+    {
+        animator.SetBool("IsJumping", false);
+    }
+
+    public void OnCrouching(bool isCrouching)
+    {
+        animator.SetBool("IsCrouching", isCrouching);
     }
 
      void FixedUpdate()
     {
-        controller.Move(horizontalMove * Time.fixedDeltaTime, false, false);
+        controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
+        jump = false;
     }
 }
