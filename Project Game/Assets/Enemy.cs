58e0573cc63d97fe27abff27526595a1b820eb8e
CaptainKnobel <cklein@live.at>
2023-04-16 12:50:22 +0200

    Added Killable Enemy
    Enemy has physics, can take damage, can die, has animations.
    When dead their physics get removed and they stay dead.
    + Player character has now working death animation too (no death script or HP yet)

---
+++ Project Game/Assets/Enemy.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public Animator animator;
+
+    public int maxHealth = 100;
+    int currentHealth;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    /*
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    */
+
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+        Debug.Log("Enemy took " + damage + ".");
+
+        //Play hurt animation
+        animator.SetTrigger("Hurt");
+
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        Debug.Log("Enemy died!");
+
+        // Die animation
+        animator.SetBool("IsDead", true);
+
+        // Disable the enemy
+        GetComponent<Collider2D>().enabled = false;
+        this.enabled = false;
+    }
+
+}
