63e0a30223c9ad027aef4b6d7f0c232adb72ed20
CaptainKnobel <cklein@live.at>
2023-05-12 19:29:44 +0200

    Functioning HP Bar
    Replaced the HP bar with a better one, included a User Interface Library for it, which can later be used for menus too.
    Renamed the testing level from Sample Scene to TestLevel1, created TestLevel2 for functionality testing.
    Rearranged folders, most Sprites are now found in the "Sprites" folder.
    Subfolder "User Interface" holds everything regarding the UI and Menu Icons.

--- Project Games/Assets/Health.cs
+++ Project Games/Assets/Health.cs
@@ -4,15 +4,47 @@ using UnityEngine;
 
 public class Health : MonoBehaviour
 {
+    public int maxHealth = 100;
+    public int currentHealth;
+
+    public HealthBar healthBar;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        currentHealth = maxHealth;
+        healthBar.SetMaxHealth(maxHealth);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            TakeDamage(20);
+        }
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            HealDamage(10);
+        }
+    }
+
+    void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+        healthBar.SetHealth(currentHealth);
+    }
+
+    void HealDamage(int heal)
+    {
+        if((currentHealth + heal) > maxHealth)
+        {
+            currentHealth = maxHealth;
+        }
+        else
+        {
+            currentHealth += heal;
+        }
+        healthBar.SetHealth(currentHealth);
     }
 }
